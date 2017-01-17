a47ec0e7401d8934c131c14c13f21cc1a6316356
Daniel Terranova <daniel.terranova@gmail.com>
2017-01-17 06:56:16 +0100

    Fixes

--- src/FakeGCFServer2.c
+++ src/FakeGCFServer2.c
@@ -4,8 +4,8 @@
 
 /* include fig01 */
 #include <assert.h>
-#include	"Pratical.h"
-#include    "SoupBin.h"
+#include "Pratical.h"
+#include "SoupBin.h"
 
 #define SERV_PORT 29300
 #define MAX_CLIENTS 10
@@ -35,7 +35,7 @@ static char file[BUFSIZE];
 
 int main(int argc, char **argv)
 {
-    int					i, maxi, listenfd, connfd, sockfd;
+    int					i, maxi, listenfd, connfd, sockfd, enable = 1;
     int					nready;
     ssize_t				n;
     char				buf[MAXLINE];
@@ -54,6 +54,9 @@ int main(int argc, char **argv)
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port        = htons(SERV_PORT);
 
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+        DieWithSystemMessage("setsockopt(SO_REUSEADDR) failed");
+
     Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
     Listen(listenfd, LISTENQ);
@@ -67,7 +70,10 @@ int main(int argc, char **argv)
     for ( ; ; ) {
         nready = Poll(client, maxi+1, INFTIM);
 
-        if (client[0].revents & POLLRDNORM) {	/* new client connection */
+        // Data may read from Listening Socket..
+        // new client connection arrived
+        if (client[0].revents & POLLRDNORM)
+        {
             clilen = sizeof(cliaddr);
             connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
 
@@ -87,7 +93,9 @@ int main(int argc, char **argv)
                 continue;				/* no more readable descriptors */
         }
 
-        for (i = 1; i <= maxi; i++) {	/* check all clients for data */
+        /* check all clients for data */
+        for (i = 1; i <= maxi; i++)
+        {
             if ( (sockfd = client[i].fd) < 0)
                 continue;
             if (client[i].revents & (POLLRDNORM | POLLERR)) {
@@ -140,6 +148,54 @@ void handle(int fd,const char* buf)
 
 }
 
+void sendLine(int sockfd)
+{
+
+    int index = find(sockfd);
+
+    long seq = clients[index].seqNo;
+    FILE* fp = clients[index].fp;
+    char *line = NULL;
+    size_t l = 0;
+    ssize_t read, sent;
+    char send_buffer[1000000];
+
+
+    long fileSequence = 1;
+    while ((read = getline(&line, &l, fp)) != -1)
+    {
+        if (fileSequence++ >= seq) {
+            memset(&send_buffer, 0, sizeof(BUFSIZE));       // Zero out structure
+            int size = read;
+            assert(size > 0);
+
+            send_buffer[0] = (size >> 8) & 0xFF;
+            send_buffer[1] = size & 0xFF;
+            send_buffer[2] = 'S';
+            strncpy(send_buffer+3, line, size-1); /// Do not include trailing '\n'
+            /// size include '\n' (payload = size-1)
+            /// total package = payload+type(1)+headersize(2) = size-1+1+2)
+            Writen(sockfd, send_buffer, size+2);
+
+            if(DEBUG)
+                printf ("(seq=%ld ,sent=%zd,hdr[%d,%d,%c] payloadsize=%d) %s", fileSequence-1, sent,send_buffer[0],send_buffer[1], send_buffer[2],
+                        size, line);
+        }
+    }
+
+    if (read == -1) {
+        //char *ptr = send_buffer;
+        char buf[] = {0x00, 0x01, 'Z', '\0'};
+        Writen(sockfd, buf, 3);
+
+        printf ("(seq=%d ,sent=%zd,hdr[%d,%d,%c] payloadsize=1) '' \n", 0, sent, buf[0],buf[1], buf[2]);
+    }
+
+    printf("Total lines written %ld !\n", fileSequence-seq);
+    free(line);
+    fclose(fp);
+}
+
 void sendSequencedData(int fd)
 {
     int index = find(fd);
@@ -150,7 +206,6 @@ void sendSequencedData(int fd)
 
         sendLine(clients[index].sockfd);
     }
-
 }
 
 void OpenFd(int fd)
@@ -160,12 +215,11 @@ void OpenFd(int fd)
         exit(EXIT_FAILURE);
 
     int index = find(fd);
-    if (index > 0)
+    if (index > -1)
     {
         clients[index].fp = fp;
     }
 }
-
 
 void sendLine(int sockfd);
 
@@ -234,55 +288,3 @@ int find(int fd) {
     return -1;
 }
 
-void sendLine(int sockfd)
-{
-
-    int index = find(sockfd);
-
-    long seq = clients[index].seqNo;
-    FILE* fp = clients[index].fp;
-    char *line = NULL;
-    size_t l = 0;
-    ssize_t read, sent;
-    char send_buffer[1000000];
-
-
-    long fileSequence = 1;
-    while ((read = getline(&line, &l, fp)) != -1)
-    {
-        if (fileSequence++ >= seq) {
-            memset(&send_buffer, 0, sizeof(BUFSIZE));       // Zero out structure
-            int size = read;
-            assert(size > 0);
-
-            send_buffer[0] = (size >> 8) & 0xFF;
-            send_buffer[1] = size & 0xFF;
-            send_buffer[2] = 'S';
-            strncpy(send_buffer+3, line, size-1); /// Do not include trailing '\n'
-            /// size include '\n' (payload = size-1)
-            /// total package = payload+type(1)+headersize(2) = size-1+1+2)
-            Writen(sockfd, send_buffer, size+2);
-
-            if(DEBUG)
-                printf ("(seq=%ld ,sent=%zd,hdr[%d,%d,%c] payloadsize=%d) %s", fileSequence-1, sent,send_buffer[0],send_buffer[1], send_buffer[2],
-                        size, line);
-            assert(sent == (size+2));
-        }
-    }
-
-
-
-    if (read == -1) {
-        //char *ptr = send_buffer;
-        char buf[] = {0x00, 0x01, 'Z', '\0'};
-        Writen(sockfd, buf, 3);
-
-        printf ("(seq=%d ,sent=%zd,hdr[%d,%d,%c] payloadsize=1) '' \n", 0, sent, buf[0],buf[1], buf[2]);
-    }
-
-    printf("Total lines written %ld !\n", fileSequence-seq);
-    free(line);
-    fclose(fp);
-
-    close(sockfd); // Close client socket
-}
