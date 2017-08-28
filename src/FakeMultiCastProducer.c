17a249fb2964b549ac139cdc4025833ff158defa
Daniel Terranova <daniel.terranova@gmail.com>
2017-08-28 05:52:49 +0200

    More work

--- src/FakeMultiCastProducer.c
+++ src/FakeMultiCastProducer.c
@@ -1,47 +1,71 @@
 //
 // Created by danter on 2016-10-03.
-//
-
 /*
- * sender.c -- multicasts "hello, world!" to a multicast group once a second
+ * FakeMultiCastProducer.c -- multicasts "Mitch Message" to a multicast group once a second
  *
- * Antony Courtney,	25/11/94
  */
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <time.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <Pratical.h>
+#include <errno.h>
+#include <limits.h>
 
-#define HELLO_PORT 6300
-#define HELLO_GROUP "225.0.0.37"
-
+int
 main(int argc, char *argv[])
 {
     struct sockaddr_in addr;
-    int fd;
-    struct ip_mreq mreq;
-    char *message="Hello, World!";
+    int fd, c, mc_port = 8888;
+    char *mc_addr = NULL, *end_ptr;
 
-    /** set up listening UDP Server **/
-    int sockfd; /* socket */
-    int portno; /* port to listen on */
-    int clientlen; /* byte size of client's address */
-    struct sockaddr_in serveraddr; /* server's addr */
-    struct sockaddr_in clientaddr; /* client addr */
-    struct hostent *hostp; /* client host info */
-    char buf[BUFSIZE]; /* message buf */
-    char *hostaddrp; /* dotted decimal host addr string */
-    int optval; /* flag value for setsockopt */
-    ssize_t n; /* message byte size */
 
-    /* create what looks like an ordinary UDP socket */
+    unsigned char buf[BUFSIZE] = { 0x21, 0,             // Message Unit Header total-length             2 bytes
+                                   0x02,                //                     Message count            1 byte
+                                   0x43,                //                     Market Data Group        1 byte
+                                   0x98, 0x6, 0x0, 0x0, //                     Sequence number          4 bytes
+                                   0x6,  0x54,          //      Message Header (length, type)           2 bytes
+                                   0x6d, 0xe8, 0 , 0,   //                     Time field
+                                   0x13, 0x44,          //      Message Header (length, type )          2 bytes
+                                   0x10, 0x29, 0x28, 0x1c, 0x48, 0xdd, 0, 0x40, 0x44, 0x22, 0x6, 0, 0x89, 0xc7, 0x2d, 0x00, 0x00 };
+
+
+    while ((c = getopt(argc, argv, "a:p:")) != EOF) {
+        switch (c) {
+        case 'a': mc_addr = optarg;
+            break;
+        case 'p':
+        {
+            errno = 0;    /* To distinguish success/failure after call */
+            mc_port = (int)strtol(optarg, &end_ptr, 10);
+
+            if ((errno == ERANGE && (mc_port == LONG_MAX || mc_port == LONG_MIN))
+                    || (errno != 0 && mc_port == 0)) {
+                perror("strtol");
+                exit(EXIT_FAILURE);
+            }
+
+            if (end_ptr == optarg) {
+                fprintf(stderr, "No digits were found\n");
+                exit(EXIT_FAILURE);
+            }
+
+        }
+            break;
+        default:
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind < 5) {
+        fprintf(stderr, "Usage: %s -a <ip> -p <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     if ((fd = socket(AF_INET,SOCK_DGRAM, 0)) < 0) {
         perror("socket");
         exit (1);
@@ -50,62 +74,17 @@ main(int argc, char *argv[])
     /// set up destination Multicast address
     memset(&addr, 0 , sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(HELLO_GROUP);
-    addr.sin_port = htons(HELLO_PORT);
-
-    /** check command line arguments
-     */
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
-    portno = atoi(argv[1]);
-
-    /*
-     * socket: create the parent socket
-     */
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("ERROR opening socket");
-    }
-
-    /** setsockopt: Handy debugging trick that lets
-     * us rerun the server immediately after we kill it;
-     * otherwise we have to wait about 20 secs.
-     * Eliminates "ERROR on binding: Address already in use" error.
-     */
-    optval = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
-            (const void *)&optval , sizeof(int));
-
-    /*
-     * build the server's Internet address
-     */
-    bzero((char *) &serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons((unsigned short)portno);
-
-    /*
-     * bind: associate the parent socket with a port
-     */
-    if (bind(sockfd, (struct sockaddr *) &serveraddr,
-            sizeof(serveraddr)) < 0) {
-        perror("ERROR on binding");
-    }
+    addr.sin_addr.s_addr = inet_addr(mc_addr);
+    addr.sin_port = htons(mc_port);
 
     ssize_t sent  = 0;
-    /* now just sendto() our destination! */
+    /// now just sendto() our destination!
     while (1) {
-        /*
-        * recvfrom: receive a UDP datagram from a client
-        */
-
-        if ((sent = sendto(fd, message, strlen(message), 0 , (struct sockaddr *) &addr, sizeof(addr))) < 0) {
+        if ((sent = sendto(fd, buf, 33, 0 , (struct sockaddr *) &addr, sizeof(addr))) < 0) {
             perror("sendto");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
-        printf("Send.. %zd\n", sent);
+        printf("Sending %zd bytes to %s port %d\n", sent, mc_addr, mc_port);
         sleep(1);
     }
 }
